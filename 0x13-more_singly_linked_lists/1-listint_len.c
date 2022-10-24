40c3f2f3d95834c3cbce4a57bd8b00b077be31df
Chuene <110992072+Chuene05@users.noreply.github.com>
2022-10-24 09:27:05 +0200

    tasks

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,16 @@
 #include "lists.h"
 
 /**
- * print_listint - Print all the elements of a list_int list
+ * listint_len - Return the number of elements
  * @h: data type pointer of struct
  * Return: elements of the str i
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	unsigned int i = 0;
 	const listint_t	 *new_node = h;
 
 	for (; new_node; new_node = new_node->next)
-	{
-		printf("%d\n", new_node->n);
 		i++;
-	}
-	return (i);
+		return (i);
 }
